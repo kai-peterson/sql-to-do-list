cc8b7f4ab3a0d3caa97492aae591237080115c22
Kai Peterson <pete9372@umn.edu>
2019-10-21 09:13:38 -0500

    update database.sql

--- database.sql
+++ database.sql
@@ -5,8 +5,10 @@ CREATE TABLE koalas(
 );
 
 -- test tasks to insert initially
-INSERT INTO "tasks"("task", "is_completed")
-VALUES ('test-task1', 'Y'), ('test-task2', 'N'), ('test-task3', 'Y');
+INSERT INTO "tasks"("task", "is_completed", "category")
+VALUES ('coding task one', 'false', 'coding'), ('coding task two', 'false', 'coding'), ('coding task long text i have a lot to do', 'false', 'coding'), 
+('home life task one', 'false', 'home life'), ('home life task two', 'false', 'home life'), ('home life task long text i have a lot of things to do', 'false', 'home life'), 
+('random task one', 'false', 'random'), ('random task two', 'false', 'random'), ('random task long text i have a lot of really time-consuming things to do', 'false', 'random');
 
 SELECT * FROM "tasks" ORDER BY "id";
 
