4052c387812fef9214dd46b4f3ab33b594c5809c
Kai Peterson <pete9372@umn.edu>
2019-10-21 11:24:20 -0500

    add database config for heroku

--- server/server.js
+++ server/server.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const app = express();
 const pg = require('pg');
+const pool = require('/modules/pool.js')
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 // const router = require('./routes/THISrouter')
 
-const config = {
-    database: 'weekend_to_do_app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-};
-
-const pool = new pg.Pool(config);
-
-pool.on("connect", () => {
-    console.log("connected to postgres");
-});
-
-pool.on("error", (err) => {
-    console.log("error connecting to postgres", err);
-});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
